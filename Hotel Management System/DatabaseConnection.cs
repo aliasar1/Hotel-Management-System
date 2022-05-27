6d63275b5df3632bb11e5a4609f5ecf1f212983c
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2022-05-27 23:01:12 +0500

    Added database connection.

---
+++ Hotel Management System/DatabaseConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Management_System
+{
+    internal class DatabaseConnection
+    {
+        public DatabaseConnection()
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-9GED25C;Initial Catalog=HotelManagementSystem;Integrated Security=True");
+            connection.Open();
+            SqlCommand query = new SqlCommand();
+            query.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
